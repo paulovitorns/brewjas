a5bfe8c1b4b62e1154bffee716923edc52c7f899
paulo-sales <paulo.vitor@cetax.com.br>
2016-05-17 14:07:18 -0300

    Tela de datalhes da cerveja

--- app/src/main/java/br/com/brewjas/ui/activity/BrejaActivity.java
+++ app/src/main/java/br/com/brewjas/ui/activity/BrejaActivity.java
@@ -1,17 +1,100 @@
 package br.com.brewjas.ui.activity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.graphics.Palette;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import br.com.brewjas.R;
 
 public class BrejaActivity extends AppCompatActivity {
+
+    private CollapsingToolbarLayout collapsingToolbarLayout = null;
+    private Toolbar toolbar = null;
+    private TextView descBeer = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_breja);
 
+        String desc = "<span style=\"text-align: justify;\">Sobre a <b>Guinness Draught</b>: Is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</span>";
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        descBeer = (TextView) findViewById(R.id.descBeer);
+        descBeer.setText(Html.fromHtml(desc));
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsingToolBar);
+        collapsingToolbarLayout.setTitle("Guinness Draught");
+
+        dynamicToolBarColor();
+
+        toolbarTextAppearence();
+    }
+
+    private void dynamicToolBarColor() {
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.beer_example);
+
+        Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
+            @Override
+            public void onGenerated(Palette palette) {
+                    //collapsingToolbarLayout.setContentScrimColor(palette.getMutedColor(getResources().getColor(R.color.colorPrimaryDark)));
+                    //collapsingToolbarLayout.setStatusBarScrimColor(palette.getMutedColor(getResources().getColor(R.color.colorPrimaryDark)));
+            }
+        });
 
     }
+
+    private void toolbarTextAppearence() {
+
+        collapsingToolbarLayout.setCollapsedTitleTextAppearance(R.style.collapsedappbar);
+        collapsingToolbarLayout.setExpandedTitleTextAppearance(R.style.expandedappbar);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.brejas_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case R.id.action_fav:
+                item.setIcon(getResources().getDrawable(R.drawable.ic_favorite_white_36dp));
+            break;
+
+            case android.R.id.home:
+                onBackPressed();
+            break;
+
+            default:
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
