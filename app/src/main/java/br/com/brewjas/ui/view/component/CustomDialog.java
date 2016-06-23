d4297d3b6b10e0680a8b3c42085aabad949ca2bb
Paulo Sales <paulo.sales@livetouch.com.br>
2016-06-23 16:24:31 -0300

    Custom Dialog

---
+++ app/src/main/java/br/com/brewjas/ui/view/component/CustomDialog.java
@@ -0,0 +1,51 @@
+package br.com.brewjas.ui.view.component;
+
+/*
+ * © Copyright 2015 Rede S.A.
+ * Autor : Ismael Di Vita Costa - ismael.costa@userede.com.br
+ * Empresa : Rede S.A.
+ */
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import br.com.brewjas.R;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+//how to use
+//new CustomDialog(apiError, this).show();
+
+public class CustomDialog implements View.OnClickListener{
+
+    private Dialog dialog;
+    private Context context;
+
+    public CustomDialog(Context context){
+        this.context = context;
+    }
+
+    public void show(){
+        dialog = new Dialog(context);
+
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //dialog.setContentView(R.layout.dialog_error);
+        dialog.setCancelable(false);
+
+        ButterKnife.bind(this, dialog);
+
+        //btDialog.setOnClickListener(this);
+        dialog.show();
+    }
+
+    @Override
+    public void onClick(View v) {
+        dialog.cancel();
+    }
+}
